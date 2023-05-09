e32f1c9543094fddb1f9b4ef1fb64e631e459e14
PavelChuprin <pcuprin@gmil.com>
2023-05-09 15:46:02 +0600

    Выносим форму входа в отлельный компонент

--- index.js
+++ index.js
@@ -1,78 +1,59 @@
 import { addTodo, deleteTodo, getTodos } from "./api.js";
+import { renderLoginComponent } from "./components/login-component.js"
 
 let tasks = [];
-const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 let token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
 
 token = null;
 
 const fetchTodosAndRender = () => {
-  return getTodos({token}).then((responseData) => {
+return getTodos({token}).then((responseData) => {
 		tasks = responseData.todos;
 		renderApp();
-	 });
+	});
 };
 
 const renderApp = () => {
 
 	const appEl = document.getElementById("app");
 
-  if (!token) {
-	  const appHtml = `
-<h1>Список задач</h1>
-
-<div class="form">
-  <h3 class="form-title">Форма входа</h3>
-  <div class="form-row">
-	 Логин
-	 <input type="text" id="login-input" class="input"/>
-<br></br>
-	 Пароль
-	 <input type="text" id="login-input" class="input"/>
-  </div>
-  <br />
-  <button class="button" id="login-button">Войти</button>
-</div>
-`
-
-appEl.innerHTML = appHtml;
-
-document.getElementById("login-button").addEventListener("click", () => {
-  token = 'Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k';
-  fetchTodosAndRender();
-})
-
+if (!token) {
+	renderLoginComponent({ appEl, setToken: (newToken) => {
+	token = newToken;
+	},
+	fetchTodosAndRender,
+});
 return;
-  }
+}
 
-	const tasksHtml = tasks
-	  .map((task) => {
-		 return `
-		 <li class="task">
+const tasksHtml = tasks
+	.map((task) => {
+		return `
+		<li class="task">
 			<p class="task-text">
-			  ${task.text}
-			  <button data-id="${task.id}" class="button delete-button">Удалить</button>
+			${task.text}
+			<button data-id="${task.id}" class="button delete-button">Удалить</button>
 			</p>
-		 </li>`;
-	  })
-	  .join("");
+		</li>`;
+	})
+	.join("");
 
 const appHtml = `
 <h1>Список задач</h1>
 
 <ul class="tasks" id="list">
-  <!-- <!— Список рендерится из JS —> -->
-  ${tasksHtml}
+<!-- Список рендерится из JS -->
+${tasksHtml}
 </ul>
 <br />
 <div class="form">
-  <h3 class="form-title">Форма добавления</h3>
-  <div class="form-row">
-	 Что нужно сделать:
-	 <input type="text" id="text-input" class="input" placeholder="Выпить кофе"/>
-  </div>
-  <br />
-  <button class="button" id="add-button">Добавить</button>
+	<h3 class="form-title">Форма добавления</h3>
+<div class="form-row">
+	Что нужно сделать:
+	<input type="text" id="text-input" class="input" placeholder="Выпить кофе"/>
+</div>
+<br />
+	<button class="button" id="add-button">Добавить</button>
 </div>`
 
 appEl.innerHTML = appHtml;
@@ -83,32 +64,32 @@ const textInputElement = document.getElementById("text-input");
 
 const deleteButtons = document.querySelectorAll(".delete-button");
 
-  for (const deleteButton of deleteButtons) {
-	 deleteButton.addEventListener("click", (event) => {
+for (const deleteButton of deleteButtons) {
+	deleteButton.addEventListener("click", (event) => {
 		event.stopPropagation();
 
 		const id = deleteButton.dataset.id;
 
-		// Подписываемся на успешное завершение запроса с помощью then
+// Подписываемся на успешное завершение запроса с помощью then
 		deleteTodo({token, id}).then((responseData) => {
-			 // Получили данные и рендерим их в приложении
-			 tasks = responseData.todos;
-			 renderApp();
-		  });
+			// Получили данные и рендерим их в приложении
+			tasks = responseData.todos;
+			renderApp();
+		});
 
 		renderApp();
-	 });
-  }
+	});
+}
 
-  buttonElement.addEventListener("click", () => {
-	 if (textInputElement.value === "") {
+buttonElement.addEventListener("click", () => {
+	if (textInputElement.value === "") {
 		return;
-	 }
+	}
 
-	 buttonElement.disabled = true;
-	 buttonElement.textContent = "Задача добавляется...";
+	buttonElement.disabled = true;
+	buttonElement.textContent = "Задача добавляется...";
 
-	 // Подписываемся на успешное завершение запроса с помощью then
+// Подписываемся на успешное завершение запроса с помощью then
 	addTodo({
 		text: textInputElement.value,
 		token
@@ -118,23 +99,22 @@ const deleteButtons = document.querySelectorAll(".delete-button");
 		textInputElement.value = "";
 		})
 		.then(() => {
-		  return fetchTodosAndRender();
+			return fetchTodosAndRender();
 		})
 		.then(() => {
-		  buttonElement.disabled = false;
-		  buttonElement.textContent = "Добавить";
+			buttonElement.disabled = false;
+			buttonElement.textContent = "Добавить";
 		})
 		.catch((error) => {
-		  console.error(error);
-		  alert("Кажется, у вас проблемы с интернетом, попробуйте позже");
-		  buttonElement.disabled = false;
-		  buttonElement.textContent = "Добавить";
+			console.error(error);
+			alert("Кажется, у вас проблемы с интернетом, попробуйте позже");
+			buttonElement.disabled = false;
+			buttonElement.textContent = "Добавить";
 		});
 
-	 renderApp();
-  });
+	renderApp();
+	});
 
 };
 
-//  fetchTodosAndRender();
 renderApp();
