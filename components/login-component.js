8d2fc8f314e41e869af2eb36dc0ebd2addbcd217
PavelChuprin <pcuprin@gmil.com>
2023-05-09 17:49:40 +0600

    Реализуем регистрацию

--- components/login-component.js
+++ components/login-component.js
@@ -1,4 +1,4 @@
-import { loginUser } from "../api.js";
+import { loginUser, registerUser } from "../api.js";
 
 export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
 	let isLoginMode = false;
@@ -27,30 +27,58 @@ export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
 		appEl.innerHTML = appHtml;
 	 
 		document.getElementById("login-button").addEventListener("click", () => {
-		  const login = document.getElementById("login-input").value;
-		  const password = document.getElementById("password-input").value;
-	 
-		  if (!login) {
-			 alert("Введите логин");
-			 return;
-		  }
-		  if (!password) {
-			 alert("Введите пароль");
-			 return;
-		  }
-	 
-		  loginUser({
-			 login: login,
-			 password: password,
-		  }).then((user) => {
-			 setToken(`Bearer ${user.user.token}`);
-			 fetchTodosAndRender();
-		  })
-		  .catch(error => {
-				 alert(error.message);
-		  })
-		});
-	 
+			
+		if(isLoginMode) {
+			const login = document.getElementById("login-input").value;
+			const password = document.getElementById("password-input").value;
+			if (!login) {
+			  alert("Введите логин");
+			  return;
+			}
+			if (!password) {
+			  alert("Введите пароль");
+			  return;
+			}
+			loginUser({
+			  login: login,
+			  password: password,
+			}).then((user) => {
+			  setToken(`Bearer ${user.user.token}`);
+			  fetchTodosAndRender();
+			})
+			.catch(error => {
+				  alert(error.message);
+			})
+		} else {
+			const name = document.getElementById("name-input").value;
+			const login = document.getElementById("login-input").value;
+			const password = document.getElementById("password-input").value;
+			if (!name) {
+				alert("Введите имя");
+				return;
+			 }
+			if (!login) {
+				alert("Введите логин");
+				return;
+			 }
+			 if (!password) {
+				alert("Введите пароль");
+				return;
+			 }
+			 registerUser({
+				login: login,
+				password: password,
+				name: name,
+			 }).then((user) => {
+				setToken(`Bearer ${user.user.token}`);
+				fetchTodosAndRender();
+			 })
+			 .catch(error => {
+					alert(error.message);
+			 })
+		}
+	});
+	
 		document.getElementById("toggle-button").addEventListener("click", () => {
 		 isLoginMode = !isLoginMode;
 		 renderForm();
