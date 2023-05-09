ee5f9a08145dde885ee9c963455b3977ea769fb5
PavelChuprin <pcuprin@gmil.com>
2023-05-09 16:58:06 +0600

    Оживляем форму регистрации

--- api.js
+++ api.js
@@ -1,56 +1,55 @@
 const host = "https://webdev-hw-api.vercel.app/api/v2/todos";
 
-export function getTodos({token}) {
-	return fetch(host, {
-		method: "GET",
-		headers: {
-		Authorization: token,
-		}
-	})
-		.then((response) => {
-		if (response.status === 401) {
-			throw new Error('Нет авторизации');
-		}
-			return response.json();
-		})
+export function getTodos({ token }) {
+  return fetch(host, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    if (response.status === 401) {
+      throw new Error("Нет авторизации");
+    }
+    return response.json();
+  });
 }
 
-export function deleteTodo({token, id}) {
-	return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
-		method: "DELETE",
-		headers: {
-			Authorization: token,
+export function deleteTodo({ token, id }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/todos/" + id, {
+    method: "DELETE",
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function addTodo({ text, token }) {
+  return fetch(host, {
+    method: "POST",
+    body: JSON.stringify({
+      text,
+    }),
+    headers: {
+      Authorization: token,
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+export function loginUser({ login, password }) {
+  return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
+    method: "POST",
+    body: JSON.stringify({
+      login,
+      password,
+    }),
+  }).then((response) => {
+	if (response.status === 400) {
+		throw new Error('Неверный логин или пароль')
 	}
-	})
-		.then((response) => {
-			return response.json();
-		})
-}
-
-export function addTodo({text, token}) {
-	return fetch(host, {
-		method: "POST",
-		body: JSON.stringify({
-			text,
-		}),
-		headers: {
-		Authorization: token,
-		}
-	})
-		.then((response) => {
-			return response.json();
-		})
-}
-
-export function login({login, password}) {
-	return fetch("https://webdev-hw-api.vercel.app/api/user/login", {
-		method: "POST",
-		body: JSON.stringify({
-			login,
-			password,
-		}),
-	})
-		.then((response) => {
-			return response.json();
-		})
+    return response.json();
+  });
 }
--- components/login-component.js
+++ components/login-component.js
@@ -1,7 +1,7 @@
-import { login } from "../api.js";
+import { loginUser } from "../api.js";
 
-export function renderLoginComponent ({ appEl, setToken, fetchTodosAndRender }) {
-	const appHtml = `
+export function renderLoginComponent({ appEl, setToken, fetchTodosAndRender }) {
+  const appHtml = `
 	<h1>Список задач</h1>
 	<div class="form">
 		<h3 class="form-title">Форма входа</h3>
@@ -10,23 +10,36 @@ export function renderLoginComponent ({ appEl, setToken, fetchTodosAndRender }) 
 		<input type="text" id="login-input" class="input"/>
 	<br></br>
 		Пароль
-		<input type="text" id="login-input" class="input"/>
+		<input type="password" id="password-input" class="input"/>
 	</div>
 	<br />
 	<button class="button" id="login-button">Войти</button>
-	</div>`
-	
-	appEl.innerHTML = appHtml;
-	
-	document.getElementById("login-button").addEventListener("click", () => {
-	// setToken('Bearer asb4c4boc86gasb4c4boc86g37w3cc3bo3b83k4g37k3bk3cg3c03ck4k');
+	</div>`;
 
-	login({
-		login: 'admin',
-		password: 'admin',
-	}).then((user) => {
-		setToken(`Bearer ${user.user.token}`);
-		fetchTodosAndRender();
-		})
-	})
+  appEl.innerHTML = appHtml;
+
+  document.getElementById("login-button").addEventListener("click", () => {
+    const login = document.getElementById("login-input").value;
+    const password = document.getElementById("password-input").value;
+
+    if (!login) {
+      alert("Введите логин");
+      return;
+    }
+    if (!password) {
+      alert("Введите пароль");
+      return;
+    }
+
+    loginUser({
+      login: login,
+      password: password,
+    }).then((user) => {
+      setToken(`Bearer ${user.user.token}`);
+      fetchTodosAndRender();
+    })
+	 .catch(error => {
+			alert(error.message);
+	 })
+  });
 }
